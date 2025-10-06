39e8db3b8684725001060d33103cdd32bc33e9a2
Rizkia Adhy Syahputra <rizkia.as.pac@gmail.com>
2025-10-06 12:25:40 +0900

    feat(plugins): integrate fzf-lua as Telescope alternative and enhance colorscheme config
    Added `fzf-lua` plugin with comprehensive keybindings and custom window configuration as a potential replacement for Telescope.
    Refactored colorscheme setup to include quick access mapping and improved `catppuccin` color overrides for better contrast.
    Updated `lazy-lock.json` to include new dependencies (`better-escape.nvim`, `mason-tool-installer.nvim`) and removed old test plugin references.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,59 +1,59 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	tag = "0.1.8",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	keys = {
-		-- {
-		-- 	"<leader>fF",
-		-- 	"<cmd>Telescope find_files<cr>",
-		-- 	desc = "Lists files in your current working directory, respects .gitignore",
-		-- },
-		-- { "<leader>fg", "<cmd>Telescope git_files<cr>" },
-		-- { "<leader>fg", "<cmd>Telescope live_grep<cr>" },
-		-- { "<leader>fr", "<cmd>Telescope lsp_references<cr>", desc = "Telescope all references" },
-		-- {
-		-- 	"<leader>fs",
-		-- 	"<cmd>Telescope lsp_document_symbols<cr>",
-		-- 	desc = "Lists LSP document symbols in the current buffer",
-		-- },
-		-- {
-		-- 	"<leader>fd",
-		-- 	"<cmd>Telescope lsp_definitions<cr>",
-		-- 	desc = "Goto the definition of the word under the cursor",
-		-- },
-		-- {
-		-- 	"<leader>fx",
-		-- 	"<cmd>Telescope diagnostics<cr>",
-		-- 	desc = "Lists Diagnostics for all open buffers or a specific buffer",
-		-- },
-		--
-		-- { "<leader>muC", "<cmd>Telescope colorscheme<cr>", desc = "Lists Colorscheme" },
-	},
-	opts = function()
-		require("telescope").setup({
-			builtin = {
-				diagnostics = {
-					previewer = false,
-				},
-			},
-			defaults = {
-				path_display = { truncate = 2 },
-				-- path_display =  "truncate"
-			},
-			pickers = {
-				colorscheme = {
-					enable_preview = true,
-				},
-			},
-			extensions = {
-				fzf = {}, -- use native fzf to make it better and faster
-			},
-		})
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.8",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  keys = {
+    -- {
+    -- 	"<leader>fF",
+    -- 	"<cmd>Telescope find_files<cr>",
+    -- 	desc = "Lists files in your current working directory, respects .gitignore",
+    -- },
+    -- { "<leader>fg", "<cmd>Telescope git_files<cr>" },
+    -- { "<leader>fg", "<cmd>Telescope live_grep<cr>" },
+    -- { "<leader>fr", "<cmd>Telescope lsp_references<cr>", desc = "Telescope all references" },
+    -- {
+    -- 	"<leader>fs",
+    -- 	"<cmd>Telescope lsp_document_symbols<cr>",
+    -- 	desc = "Lists LSP document symbols in the current buffer",
+    -- },
+    -- {
+    -- 	"<leader>fd",
+    -- 	"<cmd>Telescope lsp_definitions<cr>",
+    -- 	desc = "Goto the definition of the word under the cursor",
+    -- },
+    -- {
+    -- 	"<leader>fx",
+    -- 	"<cmd>Telescope diagnostics<cr>",
+    -- 	desc = "Lists Diagnostics for all open buffers or a specific buffer",
+    -- },
+    --
+    -- { "<leader>muC", "<cmd>Telescope colorscheme<cr>", desc = "Lists Colorscheme" },
+  },
+  opts = function()
+    require("telescope").setup({
+      builtin = {
+        diagnostics = {
+          previewer = false,
+        },
+      },
+      defaults = {
+        path_display = { truncate = 2 },
+        -- path_display =  "truncate"
+      },
+      pickers = {
+        colorscheme = {
+          enable_preview = true,
+        },
+      },
+      extensions = {
+        fzf = {}, -- use native fzf to make it better and faster
+      },
+    })
 
-		vim.keymap.set("n", "<leader>mlp", function()
-			require("telescope.builtin").find_files({
-				cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy"),
-			})
-		end)
-	end,
+    vim.keymap.set("n", "<leader>mlp", function()
+      require("telescope.builtin").find_files({
+        cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy"),
+      })
+    end)
+  end,
 }
--- lua/plugins/ui/colorscheme.lua
+++ lua/plugins/ui/colorscheme.lua
@@ -1,37 +1,48 @@
 return {
-	{
-		"folke/tokyonight.nvim",
-		lazy = true,
-		opts = {
-			style = "moon",
-			integrations = { blink_cmp = true },
-		},
-	},
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		opts = {
-			-- flavour = "latte",
-			flavour = "macchiato",
-			color_overrides = {
-				macchiato = {
-					overlay2 = "#9399b2",
-					overlay1 = "#7f849c",
-					overlay0 = "#6c7086",
-					surface2 = "#585b70",
-					surface1 = "#45475a",
-					-- surface0 = "#313244", -- ori
-					surface0 = "#272735", -- bit darker
-					base = "#1e1e2e",
-					mantle = "#181825",
-					crust = "#11111b",
-				},
-				latte = {
-					green = "#439d2f",
-				},
-			},
-			-- ref : catppuccin/nvim/lua/catppuccin/palettes/latte.lua
-		},
-	},
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = {
+      style = "moon",
+      integrations = { blink_cmp = true },
+    },
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      -- QUICK ACCESS TO PLUGIN SPECIFIC CONFIG
+      vim.keymap.set("n", "<leader>muc", "<cmd>exe 'edit' stdpath('config').'/lua/plugins/ui/colorscheme.lua'<cr>",
+        { desc = "colorscheme config" })
+
+      require("catppuccin").setup({
+        -- flavour = "latte",
+        flavour = "macchiato",
+        color_overrides = {
+          macchiato = {
+            overlay2 = "#9399b2",
+            overlay1 = "#7f849c",
+            overlay0 = "#6c7086",
+            surface2 = "#585b70",
+            surface1 = "#45475a",
+            -- surface0 = "#313244", -- ori
+            surface0 = "#272735", -- bit darker
+            base = "#1e1e2e",
+            mantle = "#181825",
+            crust = "#11111b",
+          },
+          latte = {
+            green = "#158995", -- blueish green  # 1
+            -- green = "#006600", -- darker green #2
+            -- green = "#016342", -- blueish green #4
+            -- green = "#098baa", -- blueish green #3
+            base = "#e4e4e4",
+            mantle = "#d7d7d7",
+          },
+        },
+        -- ref : catppuccin/nvim/lua/catppuccin/palettes/latte.lua
+      })
+    end,
+  },
 }
