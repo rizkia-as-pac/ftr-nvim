54985754f48ef814bd6c42314f313e70dbd07088
Rizkia Adhy Syahputra <rizkia.as.pac@gmail.com>
2026-02-07 22:02:06 +0900

    feat(install): add lsof package and avoid sudo for user config setup
    - Add lsof to the list of Arch official packages
    - Remove sudo usage when creating and copying Neovim config files
    - Ensure Neovim configuration is installed with correct user ownership

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@ sudo pacman -S --needed --noconfirm - <./arch-official-packages.txt
 
 mv ./exclude-config.sh ./config.sh
 
-sudo mkdir -p $HOME/.config/nvim
-sudo cp -r ./lazyvim.json $HOME/.config/nvim/
-sudo cp -r ./lazy-lock.json $HOME/.config/nvim/
-sudo cp -r ./init.lua $HOME/.config/nvim/
-sudo cp -r ./lua $HOME/.config/nvim/
+mkdir -p $HOME/.config/nvim
+cp -r ./lazyvim.json $HOME/.config/nvim/
+cp -r ./lazy-lock.json $HOME/.config/nvim/
+cp -r ./init.lua $HOME/.config/nvim/
+cp -r ./lua $HOME/.config/nvim/
